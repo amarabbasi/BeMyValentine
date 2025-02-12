27f1e8e0200c5e63e7c1500d9ca01407526a83cf
amarabbasi <amarabbasid@gmail.com>
2025-02-12 16:42:54 +0400

    Fixed falling hearts animation

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function createHearts(numbers) {
     for (var i = 0; i < numbers; i++) {
         var heart = document.createElement("img");
 
-        heart.src = "assets/Heart_corazon.svg.png";
+        heart.src = "assets/heart2.png";
         heart.alt = "Heart";
         heart.classList.add("heart");
         heart.classList.add("heart" + (i % 3 + 1)); // Assign different animation classes
